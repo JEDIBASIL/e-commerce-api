d83c5fd653aae9d0037222419b5a37c5d7715b96
JEDIBASIL <jedidiahbasil@gmail.com>
2023-01-02 06:30:49 +0100

    created user interface

---
+++ src/interface/model/user.model.interface.ts
@@ -0,0 +1,14 @@
+import { BlobOptions } from "buffer";
+
+interface IUser {
+    username: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    password: string;
+    isVerified: boolean;
+    status: boolean;
+    joinedAt: Date
+}
+
+export default IUser
