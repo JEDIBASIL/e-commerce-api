16d9566c6dffdd55721ccb427967dc7d59b3b3fd
JEDIBASIL <jedidiahbasil@gmail.com>
2023-01-02 06:36:40 +0100

    created order schema

---
+++ src/models/order.model.ts
@@ -0,0 +1,76 @@
+import moment from 'moment';
+import mongoose, { model, Schema, Document } from 'mongoose';
+import {IOrder} from '../interface/';
+
+const orderSchema = new Schema<IOrder>({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "User"
+    },
+    orderItems: [
+        {
+            name: { type: String, required: true },
+            qty: { type: Number, required: true },
+            img: { type: String, required: true },
+            price: { type: Number, required: true },
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                required: true,
+                ref: "Product"
+            }
+        }
+    ],
+    shippingAddress: {
+        address: { type: String, required: true },
+        city: { type: String, required: true },
+        postalCode: { type: String, required: true },
+        country: { type: String, required: true }
+    },
+    paymentResult: {
+        id: {
+            type: String
+        },
+        status: {
+            type: String
+        },
+        update_time: {
+            type: String
+        },
+        email: {
+            type: String
+        },
+    },
+    taxPrice: {
+        type: Number,
+        required: true,
+        default: 0.0
+    },
+    shippingPrize: {
+        type: Number,
+        required: true,
+        default: 0.0
+    },
+    totalPrize: {
+        type: Number,
+        required: true,
+        default: 0.0
+    },
+    isPaid: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    paidAt: {
+        type: Date,
+    },
+    isDelivered: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    orderedAt: {
+        type: Date,
+        default: () => moment().toDate()
+    },
+})
