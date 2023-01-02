924c0f5cfdf69844ed097f238aa1147562909312
JEDIBASIL <jedidiahbasil@gmail.com>
2023-01-02 06:33:56 +0100

    created interface index file

---
+++ src/interface/index.ts
@@ -0,0 +1,5 @@
+import IUser from "./model/user.model.interface"
+import IOrder from "./model/order.model.interface"
+import IProduct from "./model/products.model.interface"
+import IReview from "./model/review.model.interface"
+export { IUser, IOrder, IProduct, IReview }
