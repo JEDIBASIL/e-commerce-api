080132932c8979cccac73abae4695ded650849af
JEDIBASIL <jedidiahbasil@gmail.com>
2023-01-03 11:37:19 +0100

    created category model

---
+++ src/models/category.model.ts
@@ -0,0 +1,19 @@
+import moment from "moment";
+import { Schema, model } from "mongoose";
+import { ICategory } from "../interface";
+
+const categorySchema = new Schema<ICategory>({
+    name:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    addedAt:{
+        type:Date,
+        default: () => moment().toDate()
+    }
+})
+
+const categoryModel = model<Document & ICategory>("Category", categorySchema)
+
+export default categoryModel
