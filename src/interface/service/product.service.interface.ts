ffba34d5e96f097f96afab5e5961798dc57d0a9a
JEDIBASIL <jedidiahbasil@gmail.com>
2023-01-03 18:08:29 +0100

    added interface for category

--- src/interface/service/product.service.interface.ts
+++ src/interface/service/product.service.interface.ts
@@ -1,4 +1,5 @@
-import { AddProductDto, DeleteProductDto, GetProductDto, UpdateProductDto } from "../../dto/product.dto";
+import { AddProductDto, DeleteProductDto, GetProductDto, UpdateProductDto, CreateCategoryDto } from "../../dto/product.dto";
+import ICategory from "../model/category.model.interface";
 import IProduct from "../model/products.model.interface";
 
 
@@ -7,13 +8,17 @@ import IProduct from "../model/products.model.interface";
 // delete product
 // update product
 // add product
+// add category
+// get categories
 
 interface IProductService {
     addProduct(newProduct: AddProductDto): Promise<IProduct>;
     getAllProducts(): Promise<IProduct[]>;
     getProduct(id: GetProductDto): Promise<AddProductDto>;
     deleteProduct(id: DeleteProductDto): Promise<Boolean>;
-    updateProduct(product: UpdateProductDto,id:string): Promise<Boolean>;
+    updateProduct(product: UpdateProductDto, id: string): Promise<Boolean>;
+    addCategory(category: CreateCategoryDto): Promise<ICategory>;
+    getCategories(): Promise<ICategory[]>;
 }
 
 export default IProductService
