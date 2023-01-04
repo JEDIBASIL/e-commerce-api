144d1b3dd45f68b0ab4fb452176679e8477d4664
JEDIBASIL <jedidiahbasil@gmail.com>
2023-01-04 23:12:43 +0100

    created a function to read the mail template

---
+++ src/utils/templateReader.ts
@@ -0,0 +1,18 @@
+import fs from "fs"
+import util from "util"
+import Handlebars from "handlebars"
+
+const templateReader = async (file: string, info?: any): Promise<string> => {
+    let templateFile = ""
+    try {
+        const readFile = util.promisify(fs.readFile);
+        templateFile = await readFile(`${process.cwd()}/src/upload/templates/${file}`, "utf-8")
+    } catch (err: unknown) {
+        if (err instanceof Error) throw err
+    }
+    const template = Handlebars.compile(templateFile)
+    return template({ ...info });
+
+}
+
+export { templateReader }
