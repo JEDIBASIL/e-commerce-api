2e7c3c6fd9487b7b89ec346d7518bc9b5855bbf9
JEDIBASIL <jedidiahbasil@gmail.com>
2023-01-04 19:13:13 +0100

    added mail credentials

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,3 @@
 import { config } from "dotenv"
 config()
-export const { PORT, DATABASE_URL, ACCESS_TOKEN, SECRET_TOKEN } = process.env
+export const { PORT, DATABASE_URL, ACCESS_TOKEN, SECRET_TOKEN, MAIL_PASS, MAIL_SERVICE, MAIL_USER } = process.env
