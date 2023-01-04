1926ff8820d000f27e305a0dda1f1d58ff0552f3
JEDIBASIL <jedidiahbasil@gmail.com>
2023-01-04 23:13:45 +0100

    added functionality to send mail after creating account

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,12 +1,16 @@
-import { NextFunction, Request, Response, Router } from "express";
+import { NextFunction, Request, Response } from "express";
 import UserService from "../service/user.service";
 import HttpResponse from "../response/HttpResponse";
 import HttpException from "../exceptions/HttpException";
 import CreateAccountDto from "../dto/user.dto";
-
+import Mail from "../utils/mail";
+import MailOptions from "../utils/mailOptions";
+import { templateReader } from "../utils/templateReader";
+// import ok from "../upload"
 class UserController {
 
     private service = new UserService();
+    private mail = new Mail();
 
     createAccount = async (req: Request, res: Response, next: NextFunction) => {
         try {
@@ -14,6 +18,8 @@ class UserController {
             if (data === undefined) throw new HttpException(400, "all fields are required")
             const newAccount = await this.service.createAccount(data);
             if (!newAccount) return res.status(500).send(new HttpResponse("failed", "an error occurred"))
+            const emailTemplate = await templateReader(`verifyMail.hbs`, { username: data.username, link: data.email })
+            await this.mail.sendMail(new MailOptions(data.email, "verify account", emailTemplate))
             return res.status(200).send(new HttpResponse("success", "account created successfully"))
         } catch (err: unknown) {
             if (err instanceof Error) next(err)
