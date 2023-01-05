a2815ac96b323238d29af942042501f9c2b16149
JEDIBASIL <jedidiahbasil@gmail.com>
2023-01-05 01:18:18 +0100

    created a service method to verfiy user account

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -4,6 +4,7 @@ import { IUser, IUserService } from "../interface";
 import userModel from "../models/user.model";
 
 class UserService implements IUserService {
+
     private model = userModel;
 
     loginAccount(): Promise<IUser> {
@@ -15,12 +16,21 @@ class UserService implements IUserService {
         return users
     }
     async createAccount(newUser: CreateAccountDto): Promise<CreateAccountDto> {
-        const searchedEmail = await this.model.findOne({ email:newUser.email })
-        const searchedUsername = await this.model.findOne({ username:newUser.username })
+        const searchedEmail = await this.model.findOne({ email: newUser.email })
+        const searchedUsername = await this.model.findOne({ username: newUser.username })
         if (searchedUsername) throw new HttpException(409, "username taken")
         if (searchedEmail) throw new HttpException(409, "email taken")
-        const newAccount = await this.model.create({ ...newUser }) 
+        const newAccount = await this.model.create({ ...newUser })
         return newAccount;
+    }
+
+    async verify(value: string): Promise<Boolean> {
+        const account = await this.model.findOne({ email: value })
+        if (!account) throw new HttpException(400, "invalid token")
+        if (account.isVerified) throw new HttpException(200, "user is already verified")
+        account.isVerified = true;
+        account.save();
+        return true;
     }
 }
 
