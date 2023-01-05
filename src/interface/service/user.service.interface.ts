205077ba041c52e67b5c8cad04b585f5bb465216
JEDIBASIL <jedidiahbasil@gmail.com>
2023-01-05 01:16:57 +0100

    updated user service interface

--- src/interface/service/user.service.interface.ts
+++ src/interface/service/user.service.interface.ts
@@ -1,10 +1,17 @@
 import CreateAccountDto from "../../dto/user.dto";
 import IUser from "../model/user.model.interface";
 
+// get all user
+// create account
+// login
+// verify
+// change password
+// update info
 interface IUserService{
     getAllAccount():Promise<IUser[]>;
     createAccount(newUser:CreateAccountDto): Promise<CreateAccountDto>;
     loginAccount():Promise<IUser>
+    verify(value:string):Promise<Boolean>
 }
 
 export default IUserService;
