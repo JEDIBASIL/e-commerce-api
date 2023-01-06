2b26b551a334edb993d7296bf0ca69a90ae88fce
JEDIBASIL <jedidiahbasil@gmail.com>
2023-01-06 03:42:52 +0100

    create a base auth to validate if there's token and verify it

---
+++ src/auth/base.auth.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from 'express';
+import JwtToken from '../utils/token';
+import HttpException from '../exceptions/HttpException';
+class BaseAuth {
+    protected token: string;
+    protected value: string;
+    private jwt = new JwtToken()
+    constructor(req: Request, res: Response) {
+        this.token = req.headers.authorization?.split(' ')[0] as string
+        this.value = this.jwt.verifyJwt(this.token)["value"]
+    }
+    static async check(req: Request, res: Response, next: NextFunction) {
+        try {
+            const token = req.headers.authorization
+            if (token) {
+                return next();
+            }
+            throw new HttpException(401, "token required")
+        } catch (err: unknown) {
+            if (err instanceof Error) next(err)
+        }
+    }
+}
+export default BaseAuth
