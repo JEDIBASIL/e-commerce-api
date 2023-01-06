94245a771ae02d2576a2a15a1e2a492ad6ba1e6e
JEDIBASIL <jedidiahbasil@gmail.com>
2023-01-06 03:47:25 +0100

    made the methods synchronious

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -2,12 +2,12 @@ import jwt from "jsonwebtoken"
 import { ACCESS_TOKEN } from "../config"
 
 class JwtToken {
-    async signJwt(value: string, expiresIn:string|number) {
+     signJwt(value: string, expiresIn:string|number) {
         return jwt.sign({value}, ACCESS_TOKEN as unknown as string, { expiresIn });
     }
-    async verifyJwt(value: string) {
+     verifyJwt(value: string) {
         return jwt.verify(value, ACCESS_TOKEN as unknown as string)
     }
 }
-
+ 
 export default JwtToken
