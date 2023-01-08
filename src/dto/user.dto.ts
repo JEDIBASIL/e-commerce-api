9ecf38c57ae3f9262d054f243bd1dff5ff17eae2
JEDIBASIL <jedidiahbasil@gmail.com>
2023-01-09 00:50:44 +0100

    code clean up

--- src/dto/user.dto.ts
+++ src/dto/user.dto.ts
@@ -1,27 +1,27 @@
 import { Trim } from "class-sanitizer";
-import { IsEmail, IsString, IsStrongPassword, MinLength, minLength } from "class-validator";
+import { IsEmail, IsString, MinLength } from "class-validator";
 
 class CreateAccountDto {
     @IsString()
     @Trim()
-    @MinLength(3,{message:"username should be minimum of 3 characters"})
+    @MinLength(3, { message: "username should be minimum of 3 characters" })
     public username!: string;
     @IsEmail()
     @Trim()
     public email!: string;
     @IsString()
-    @MinLength(6,{message:"minimum character should be 6"})
+    @MinLength(6, { message: "minimum character should be 6" })
     public password!: string;
 }
 
 class UpdateInfoDto {
     @IsString()
     @Trim()
-    @MinLength(3,{message:"username should be minimum of 3 characters"})
+    @MinLength(3, { message: "username should be minimum of 3 characters" })
     public firstName!: string;
     @IsString()
     @Trim()
-    @MinLength(3,{message:"username should be minimum of 3 characters"})
+    @MinLength(3, { message: "username should be minimum of 3 characters" })
     public lastName!: string;
 }
 
