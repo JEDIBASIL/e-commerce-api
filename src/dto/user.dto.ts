60e3526c5ec48dd051b5a981c399500f425db610
JEDIBASIL <jedidiahbasil@gmail.com>
2023-01-08 23:55:20 +0100

    added class validator decorator to CreateAccountDto, LoginDto, UpdateInfoDto

--- src/dto/user.dto.ts
+++ src/dto/user.dto.ts
@@ -1,16 +1,35 @@
+import { Trim } from "class-sanitizer";
+import { IsEmail, IsString, IsStrongPassword, MinLength, minLength } from "class-validator";
+
 class CreateAccountDto {
+    @IsString()
+    @Trim()
+    @MinLength(3,{message:"username should be minimum of 3 characters"})
     public username!: string;
+    @IsEmail()
+    @Trim()
     public email!: string;
+    @IsString()
+    @MinLength(6,{message:"minimum character should be 6"})
     public password!: string;
 }
 
 class UpdateInfoDto {
+    @IsString()
+    @Trim()
+    @MinLength(3,{message:"username should be minimum of 3 characters"})
     public firstName!: string;
+    @IsString()
+    @Trim()
+    @MinLength(3,{message:"username should be minimum of 3 characters"})
     public lastName!: string;
 }
 
 class LoginDto {
+    @IsString()
+    @Trim()
     public usernameOrEmail!: string;
+    @IsString()
     public password!: string;
 }
 
