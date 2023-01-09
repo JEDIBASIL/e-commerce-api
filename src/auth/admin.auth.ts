50d1f8e6ea91c290a2a7b30cfdcbc2f09b81b595
JEDIBASIL <jedidiahbasil@gmail.com>
2023-01-09 21:09:57 +0100

    added admin auth

--- src/auth/admin.auth.ts
+++ src/auth/admin.auth.ts
@@ -1,9 +1,31 @@
+import { Model, Document } from "mongoose";
+import HttpException from "../exceptions/HttpException";
 import BaseAuth from "./base.auth";
 import { NextFunction, Request, Response } from 'express';
+import adminModel from "../models/admin.model";
+import { IAdmin } from "../interface";
 
-class AdminAuth extends BaseAuth{
-    constructor(req:Request,res:Response){
-        super(req,res)
+class AdminAuth extends BaseAuth {
+    private model: Model<Document<any, any, any> & IAdmin, {}, {}, {}, any>;
+    constructor(req: Request, res: Response) {
+        super(req, res)
+        this.model = adminModel;
     }
-    
+    static async createInstance(req: Request, res: Response, next: NextFunction) {
+        try {
+            const admin = await new AdminAuth(req, res).isExist();
+            req["admin"] = admin;
+            return next()
+        } catch (e) {
+            next(e);
+        }
+    }
+    async isExist() {
+        const admin = await this.model.findOne({ username: this.value })
+        if (!admin) throw new HttpException(409, "account not found")
+        return admin
+    }
+
 }
+
+export default AdminAuth
