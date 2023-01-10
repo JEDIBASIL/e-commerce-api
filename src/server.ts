b535e3ade0459705fe7cc5d2bc7c3522914f9771
JEDIBASIL <jedidiahbasil@gmail.com>
2023-01-10 08:59:44 +0100

    added admin route

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import App from "./app";
+import AdminRoute from "./routes/admin.route";
 import ProductRoute from "./routes/product.route";
 import UserRoute from "./routes/user.route";
 
 const app = new App([
     new UserRoute(),
-    new ProductRoute()
+    new ProductRoute(),
+    new AdminRoute(),
 ]);
 
 app.listen()
