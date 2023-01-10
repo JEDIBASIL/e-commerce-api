6685a9574ccc4fc940492bf8873c8de9e326aafb
JEDIBASIL <jedidiahbasil@gmail.com>
2023-01-10 10:21:02 +0100

    added dto validation for the admin endpoints

--- src/routes/admin.route.ts
+++ src/routes/admin.route.ts
@@ -1,6 +1,9 @@
 import AdminController from "../controller/admin.controller";
 import { IRoute } from "../interface";
 import { Router } from "express";
+import dtoValidationMiddleware from "../middleware/dto.validation.middleware";
+import { AddAdminDto, AdminLoginDto, ChangePasswordDto } from "../dto/admin.dto";
+import ErrorMessage from "../enums/error.message.enum";
 class AdminRoute implements IRoute {
     path: string = "/admin";
     route: Router = Router();
@@ -11,14 +14,17 @@ class AdminRoute implements IRoute {
     private initializeRoute() {
         this.route.post(
             `${this.path}`,
+            dtoValidationMiddleware(AddAdminDto,"body",ErrorMessage.FIELDS),
             this.controller.addAdmin
         )
         this.route.post(
             `${this.path}/password`,
+            dtoValidationMiddleware(ChangePasswordDto,"body",ErrorMessage.FIELDS),
             this.controller.changePassword
         )
         this.route.post(
             `${this.path}/login`,
+            dtoValidationMiddleware(AdminLoginDto,"body",ErrorMessage.FIELDS),
             this.controller.login
         )
     }
