9c1b476b118e64017f63c6fba5f3af0d2b94958e
JEDIBASIL <jedidiahbasil@gmail.com>
2023-01-12 18:16:22 +0100

    fixed error

--- src/interface/service/admin.service.interface.ts
+++ src/interface/service/admin.service.interface.ts
@@ -1,11 +1,15 @@
-import { AddAdminDto, AdminLoginDto, BlockAdmin as BlockAdminDto, ChangePasswordDto, UnblockAdminDto } from "../../dto/admin.dto";
+import { AddAdminDto, AdminLoginDto, BlockAdminDto, ChangePasswordDto, UnblockAdminDto } from "../../dto/admin.dto";
+import { AddTemplateDto } from "../../dto/template.dto";
 import IAdmin from "../model/admin.model.interface";
+import ITemplate from "../model/template.model.interface";
 // add admin
 // block admin
 // unblock admin
 // get all admin
 // delete admin
 // change password
+// added mail template
+// delete mail template
 
 interface IAdminService {
     addAdmin(superAdmin: string, newAdmin: AddAdminDto): Promise<IAdmin>
@@ -13,6 +17,7 @@ interface IAdminService {
     login(credentials:AdminLoginDto) : Promise<IAdmin>
     block(admin:BlockAdminDto):Promise<boolean>
     unblock(admin:UnblockAdminDto):Promise<boolean>
+    addTemplate(template:AddTemplateDto):Promise<boolean>
 }
 
 export default IAdminService;
